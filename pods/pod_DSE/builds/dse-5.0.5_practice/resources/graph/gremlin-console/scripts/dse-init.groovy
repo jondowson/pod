60d5fb86af09585d9146eac69de19a313d92d760
Jon Dowson <jondowson@gmail.com>
2017-11-27 22:14:38 +0000

    remove resources

--- pods/pod_DSE/builds/dse-5.0.5_practice/resources/graph/gremlin-console/scripts/dse-init.groovy
+++
@@ -1,5 +0,0 @@
-import com.datastax.bdp.graph.api.query.Geo
-import com.datastax.bdp.graph.api.query.Search
-:remote connect tinkerpop.server conf/remote.yaml session-managed
-:remote config timeout max
-:remote console
