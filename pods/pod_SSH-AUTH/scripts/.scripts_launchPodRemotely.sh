f4a092969c998cb6ec66c8dd6d33b7cee1f95992
Jon Dowson <jondowson@gmail.com>
2018-01-03 09:17:12 +0000

    update

---
+++ pods/pod_SSH-AUTH/scripts/.scripts_launchPodRemotely.sh
@@ -0,0 +1,87 @@
+#!/usr/local/bin/bash
+
+# author:        jondowson
+# about:         script run on each server to install configured software
+
+# ------------------------------------------
+
+# uncomment to see full bash trace (debug)
+# set -x
+
+# ------------------------------------------
+
+## determine OS of this computer
+
+os=$(uname -a)
+if [[ ${os} == *"Darwin"* ]]; then
+  os="Mac"
+elif [[ ${os} == *"Ubuntu"* ]]; then
+  os="Ubuntu"
+elif [[ "$(cat /etc/system-release-cpe)" == *"centos"* ]]; then
+  os="Centos"
+elif [[ "$(cat /etc/system-release-cpe)" == *"redhat"* ]]; then
+  os="Redhat"
+else
+  os="Bad"
+  lib_generic_display_msgColourSimple "ERROR-->" "OS Not Supported"
+  exit 1;
+fi
+
+# ------------------------------------------
+
+## determine this scripts' folder path
+
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd ${parent_path}
+cd ../../../
+pod_home_path="$(pwd)"
+source "${pod_home_path}/misc/.suitcase"
+
+# ------------------------------------------
+
+## source pod_ + pod_DSE lib scripts
+
+files="$(find ${pod_home_path}/pods/pod_/lib -name "*.bash")"
+for file in $(printf "%s\n" "$files"); do
+    [ -f $file ] && . $file
+done
+
+files="$(find ${pod_home_path}/pods/pod_/prepare -name "*.bash")"
+for file in $(printf "%s\n" "$files"); do
+    [ -f $file ] && . $file
+done
+
+files="$(find ${pod_home_path}/pods/pod_REMOVE-PODS/lib/ -name "*.bash")"
+for file in $(printf "%s\n" "$files"); do
+    [ -f $file ] && . $file
+done
+
+# ------------------------------------------
+
+## source the pod-specific 'builds' folder to use
+
+source ${build_folder_path}build_settings.bash
+
+# folder specified at top of this script
+build_file_folder="${build_folder_path}"
+build_file_path="${build_file_folder}build_settings.bash"
+if [[ -f ${build_file_path} ]]; then
+  source ${build_file_path}
+else
+  lib_generic_checks_fileExists "scripts_launchPodRemotely#1" "true" "${build_file_path}"
+fi
+
+# ------------------------------------------
+
+## remove this pod and any environment settings from this server
+
+# [1] delete any previous pod build folder with the same name + any agent folder of the same version
+
+lib_doStuff_remotely_removeThisPod
+lib_doStuff_remotely_removeThisPodFromBashProfile
+lib_doStuff_remotely_removeThisPodFromBashrc
+
+# -----
+
+# [2] tidy up
+prepare_generic_misc_clearTheDecks
---
+++ pods/pod_SSH-AUTH/stages/stage_remove_pod.bash
@@ -0,0 +1,86 @@
+# author:        jondowson
+# about:         start dse on each server based on its server json defined mode
+
+# -------------------------------------------
+
+function task_removePod(){
+
+## for each server stop dse based on its json defined mode
+
+for id in $(seq 1 ${numberOfServers});
+do
+
+  tag=$(cat ${servers_json_path}           | ${jq_folder}jq '.server_'${id}'.tag'            | tr -d '"')
+  user=$(cat ${servers_json_path}          | ${jq_folder}jq '.server_'${id}'.user'           | tr -d '"')
+  sshKey=$(cat ${servers_json_path}        | ${jq_folder}jq '.server_'${id}'.sshKey'         | tr -d '"')
+  target_folder=$(cat ${servers_json_path} | ${jq_folder}jq '.server_'${id}'.target_folder'  | tr -d '"')
+  pubIp=$(cat ${servers_json_path}         | ${jq_folder}jq '.server_'${id}'.pubIp'          | tr -d '"')
+  prvIp=$(cat ${servers_json_path}         | ${jq_folder}jq '.server_'${id}'.prvIp'          | tr -d '"')
+
+# ----------
+
+  # add trailing '/' to path if not present
+  target_folder=$(lib_generic_strings_addTrailingSlash "${target_folder}")
+
+# ----------
+
+  lib_generic_display_msgColourSimple "INFO" "server: ${yellow}$tag${white} at address: ${yellow}$pubIp${reset}"
+
+# ----------
+
+  lib_generic_display_msgColourSimple "INFO-->" "removing pod:      ${REMOVE_POD}"
+
+# ----------
+
+  status="999"
+  if [[ "${status}" != "0" ]]; then
+    retry=1
+    until [[ "${retry}" == "6" ]] || [[ "${status}" == "0" ]]
+    do
+      ssh -q -i ${sshKey} ${user}@${pubIp} "rm -rf ${INSTALL_FOLDER}${REMOVE_POD} &>/dev/null"
+      status=${?}
+      if [[ "${status}" == "0" ]]; then
+        lib_generic_display_msgColourSimple "INFO-->" "ssh return code: ${green}${status}"
+      else
+        lib_generic_display_msgColourSimple "INFO-->" "ssh return code: ${red}${status} ${white}(retry ${retry}/5)"
+      fi
+      pod_remove_pod_error_array["${tag}"]="${status};${pubIp}"
+      ((retry++))
+    done
+    printf "%s\n"
+  fi
+done
+}
+
+# -------------------------------------------
+
+function task_removePod_report(){
+
+## generate a report of all failed ssh connectivity attempts
+
+lib_generic_display_msgColourSimple "REPORT" "STAGE SUMMARY: ${reset}Remove named pod from each server"
+
+declare -a pod_stop_dse_report_array
+count=0
+for k in "${!pod_remove_pod_error_array[@]}"
+do
+  lib_generic_strings_expansionDelimiter ${pod_remove_pod_error_array[$k]} ";" "1"
+  if [[ "${_D1_}" != "0" ]]; then
+    pod_remove_pod_fail="true"
+    pod_remove_pod_report_array["${count}"]="${yellow}${k}${white} at address ${yellow}${_D2_}${white} with error code ${red}${_D1_}${reset}"
+    (( count++ ))
+  fi
+done
+
+if [[ "${pod_stop_dse_fail}" == "true" ]]; then
+  printf "%s\n"
+  lib_generic_display_msgColourSimple "INFO-BOLD-->" "${red}Remove ${REMOVE_POD} errors report:"
+  printf "%s\n"
+  for k in "${pod_remove_pod_report_array[@]}"
+  do
+    lib_generic_display_msgColourSimple "INFO" "${cross} ${k}"
+  done
+else
+  lib_generic_display_msgColourSimple "SUCCESS" "Uninstall ${REMOVE_POD} from all servers"
+fi
+}
