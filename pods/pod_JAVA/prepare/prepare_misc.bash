b05fc14b631f39b675bc9bdb25b2de952f62cf39
Jon Dowson <jondowson@JDMacBook-2.local>
2018-01-30 09:49:30 +0000

    upodate

--- pods/pod_JAVA/prepare/prepare_misc.bash
+++ pods/pod_JAVA/prepare/prepare_misc.bash
@@ -9,7 +9,7 @@ function prepare_misc_checkFileFolderExist(){
 # if software tarball is used - then check it exists
 if [[ "${SOFTWARE_TARBALL}" != "" ]]; then
   prepare_generic_misc_checkSoftwareExists
-  lib_generic_checks_fileExists   "${WHICH_POD}_prepare_misc.bash_messageID:1" "true" "${SOFTWARE_TARBALL}"
+  lib_generic_checks_fileExists "${WHICH_POD}_prepare_misc.bash_messageID:1" "true" "${SOFTWARE_TARBALL}"
 fi
 
 # add below any other pod specific file and folder checks
