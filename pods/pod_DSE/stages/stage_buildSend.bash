6fb221cdb7fd00f5f3f78d4dee5617e51a798b58
Jon Dowson <jondowson@JDMacBook-2.local>
2018-04-17 23:56:37 +0100

    update

--- pods/pod_DSE/stages/stage_buildSend.bash
+++ pods/pod_DSE/stages/stage_buildSend.bash
@@ -11,16 +11,16 @@ do
 
   # [1] define in order locally run functions array (this array may be empty!)
   # escape any passed function parameters!
-  build_functions_array[0]="lib_doStuff_locally_cassandraEnv"
-  build_functions_array[1]="lib_doStuff_locally_jvmOptions"
-  build_functions_array[2]="lib_doStuff_locally_cassandraYaml_buildSettings"
-  build_functions_array[3]="lib_doStuff_locally_dseSparkEnv"
-  build_functions_array[4]="lib_doStuff_locally_cassandraRackDcProperties"
-  build_functions_array[5]="lib_doStuff_locally_cassandraYaml_json"
-  build_functions_array[6]="lib_generic_build_jqListToArray \"cass_data\""
-  build_functions_array[7]="lib_doStuff_locally_cassandraYaml_cassData"
-  build_functions_array[8]="lib_generic_build_jqListToArray \"dsefs_data\""
-  build_functions_array[9]="lib_doStuff_locally_dseYaml_dsefsData"
+  build_local_functions_array[0]="lib_doStuff_locally_cassandraEnv"
+  build_local_functions_array[1]="lib_doStuff_locally_jvmOptions"
+  build_local_functions_array[2]="lib_doStuff_locally_cassandraYaml_buildSettings"
+  build_local_functions_array[3]="lib_doStuff_locally_dseSparkEnv"
+  build_local_functions_array[4]="lib_doStuff_locally_cassandraRackDcProperties"
+  build_local_functions_array[5]="lib_doStuff_locally_cassandraYaml_json"
+  build_local_functions_array[6]="lib_generic_build_jqListToArray \"cass_data\""
+  build_local_functions_array[7]="lib_doStuff_locally_cassandraYaml_cassData"
+  build_local_functions_array[8]="lib_generic_build_jqListToArray \"dsefs_data\""
+  build_local_functions_array[9]="lib_doStuff_locally_dseYaml_dsefsData"
 
   # [2] call the generic buildSend task
   task_generic_buildSend
