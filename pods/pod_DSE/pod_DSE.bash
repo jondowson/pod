0a1728d8568b814f6672e1c2f9cff0df3d1b4471
jd <jondowson@semblent.com>
2018-05-06 19:32:45 +0100

    update

--- pods/pod_DSE/pod_DSE.bash
+++ pods/pod_DSE/pod_DSE.bash
@@ -26,7 +26,7 @@ declare -A arrayStopAgent     # populated in stage_rollingStop.bash
 # stopping/starting dse nodes
 if [[ "${clusterstateFlag}" == "true" ]]; then
 
-  GENERIC_stages_s_stubs_test_sshConnectivity   "1" "3"
+  GENERIC_stages_stubs_test_sshConnectivity   "1" "3"
   stage_stubs_stopStartCluster                "2" "3"
   stage_stubs_finish                          "3" "3"
 
@@ -34,11 +34,11 @@ if [[ "${clusterstateFlag}" == "true" ]]; then
 else
 
   stage_stubs_createResourcesFolder           "1" "7"
-  GENERIC_stages_s_stubs_testConnectivity       "2" "7"
-  GENERIC_stages_s_stubs_testWritePaths         "3" "7"   "${BUILDPATHS_WRITETEST}" "${JSONPATHS_WRITETEST}"   # set in build_settings.bash
-  GENERIC_stages_s_stubs_sendPodSoftware        "4" "7"
+  GENERIC_stages_stubs_testConnectivity       "2" "7"
+  GENERIC_stages_stubs_testWritePaths         "3" "7"   "${BUILDPATHS_WRITETEST}" "${JSONPATHS_WRITETEST}"   # set in build_settings.bash
+  GENERIC_stages_stubs_sendPodSoftware        "4" "7"
   stage_stubs_buildSendPod                    "5" "7"
-  GENERIC_stages_s_stubs_launchPod              "6" "7"   # this will execute non-generic functions on remote server (lib_doStuffRemotely.bash)
+  GENERIC_stages_stubs_launchPod              "6" "7"   # this will execute non-generic functions on remote server (lib_doStuffRemotely.bash)
   stage_stubs_finish                          "7" "7"
 fi
 }
